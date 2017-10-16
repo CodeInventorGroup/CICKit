25c0f93a8dd9ab72dd44d39744c03b64fba5dcfb
ManoBoo <jiachenmu@wecash.net>
2017-10-17 00:39:45 +0800

    add CICHUD - Building

--- CIComponentKit/CICCustomViews/HUD/CICHUD.h
+++ CIComponentKit/CICCustomViews/HUD/CICHUD.h
@@ -8,6 +8,75 @@
 
 #import <UIKit/UIKit.h>
 
+#import "UIView+CICConstructor.h"
+
+//MARK: Enum
+
+/// 提供两种样式， loading & toast
+typedef enum : NSUInteger {
+    CICHUDStyleLoading,
+    CICHUDStyleToast,
+} CICHUDStyle;
+
+/// 图片的位置
+typedef enum : NSUInteger {
+    CICHUDLayoutStyleLeft,  // 图片在左
+    CICHUDLayoutStyleTop,
+    CICHUDLayoutStyleRight,
+    CICHUDLayoutStyleBottom
+} CICHUDLayoutStyle;
+
+
+//MARK: View
+
+@class CICHUDConstructor;
 @interface CICHUD : UIView
+
+/// loading风格的 Size，默认为 {ScreenWidth - 100, 120}
+@property (nonatomic, assign) CGSize loadingSize;
+
+/// toast风格的 Size，默认为 {ScreenWidth - 180, 60}
+@property (nonatomic, assign) CGSize toastSize;
+
+/// 内容的边距，默认为 {20,20,20,20}
+@property (nonatomic, assign) UIEdgeInsets contentInsets;
+
+/// 是否自适应大小，默认为 FALSE
+@property (nonatomic, assign) BOOL isAutoResize;
+
+/// CICHUD的样式， 默认为Loading
+@property (nonatomic, assign) CICHUDStyle style;
+
+///
+@property (nonatomic, assign) CICHUDLayoutStyle layoutStyle;
+
+/// 模糊效果，默认为 extraLight
+@property (nonatomic, assign) UIBlurEffectStyle blurStyle;
+
+/// 提示文字
+@property (nonatomic, strong) NSAttributedString *title;
+
++ (instancetype)share;
+
+- (instancetype)initWithTitle:(NSAttributedString *)title;
+
+- (instancetype)initWithTitle:(NSAttributedString *)title LayoutStyle:(CICHUDLayoutStyle)layoutStyle;
+
+- (instancetype)initWithTitle:(NSAttributedString *)title
+                     HUDStyle:(CICHUDStyle)style
+                  LayoutStyle:(CICHUDLayoutStyle)layoutStyle
+              BlurEffectStyle:(UIBlurEffectStyle)blurStyle;
+
+- (CICHUDConstructor *)cic;
+@end
+
+
+
+//MARK: Extension
+
+@interface CICHUDConstructor<CICHUD> : CICUIViewConstructor
+
+CICConstructorBasicProperties(CICHUDConstructor)
+
 
 @end
--- CIComponentKit/CICCustomViews/HUD/CICHUD.m
+++ CIComponentKit/CICCustomViews/HUD/CICHUD.m
@@ -8,14 +8,112 @@
 
 #import "CICHUD.h"
 
-@implementation CICHUD
+#import "CICSizeMacros.h"
+#import "UILabel+CICConstructor.h"
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+@interface CICHUD()
+
+@property (nonatomic, strong) UIVisualEffectView *contentView;
+
+@property (nonatomic, strong) UILabel *titleLabel;
+
+@property (nonatomic, strong) UIActivityIndicatorView *activityView;
 
 @end
+
+@implementation CICHUD
+
+- (CICHUDConstructor *)cic {
+    return [[CICHUDConstructor alloc] initWithComponent:self];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self initProperties];
+    }
+    return self;
+}
+
+- (void)initProperties {
+    _loadingSize = CGSizeMake(CIC_SCREEN_WIDTH - 100, 120);
+    _toastSize = CGSizeMake(CIC_SCREEN_WIDTH - 180, 60);
+    _contentInsets = UIEdgeInsetsMake(20, 20, 20, 20);
+    _isAutoResize = FALSE;
+    _style = CICHUDStyleLoading;
+    _blurStyle = UIBlurEffectStyleExtraLight;
+    self.cic.size(_loadingSize);
+    
+    _contentView = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:_blurStyle]];
+    [self addSubview:_contentView];
+    
+    _titleLabel = [[UILabel alloc] init];
+    [_contentView.contentView addSubview:_titleLabel];
+    
+    _activityView = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0, 0, 34, 34)];
+    [_contentView.contentView addSubview:_activityView];
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    _contentView.cic.frame(self.bounds);
+}
+
+//TODO: .....TODO.....
+- (void)render {
+    _contentView.effect = [UIBlurEffect effectWithStyle:_blurStyle];
+    _activityView.hidden = (_style == CICHUDStyleToast);
+    
+    
+}
+
+//MARK: Functions
+
+static CICHUD *instancae;
++(instancetype)share {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instancae = [[self alloc] initWithFrame:CGRectZero];
+    });
+    return instancae;
+}
+
+- (instancetype)initWithTitle:(NSAttributedString *)title HUDStyle:(CICHUDStyle)style LayoutStyle:(CICHUDLayoutStyle)layoutStyle BlurEffectStyle:(UIBlurEffectStyle)blurStyle {
+    [self initProperties];
+    self.title = title;
+    self.style = style;
+    self.layoutStyle = layoutStyle;
+    self.blurStyle = blurStyle;
+    return self;
+}
+
+- (instancetype)initWithTitle:(NSAttributedString *)title LayoutStyle:(CICHUDLayoutStyle)layoutStyle {
+    return [self initWithTitle:title HUDStyle:CICHUDStyleLoading LayoutStyle:layoutStyle BlurEffectStyle:UIBlurEffectStyleExtraLight];
+}
+
+- (instancetype)initWithTitle:(NSAttributedString *)title {
+    return [self initWithTitle:title LayoutStyle:CICHUDLayoutStyleLeft];
+}
+@end
+
+
+//MARK: Extension
+
+@interface CICHUDConstructor ()
+
+@property (nonatomic, weak) CICHUD *component;
+
+@end
+
+@implementation CICHUDConstructor
+
+CICConstructorBasicDynamics()
+
+- (void)buildConstructor {
+    [super buildConstructor];
+    
+    __weak typeof(&*self) weakSelf = self;    
+}
+@end
+
