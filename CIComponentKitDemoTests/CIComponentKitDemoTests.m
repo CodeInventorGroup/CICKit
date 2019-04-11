ca8fd77764b19338d380557bfdf9495de280cd14
NEWWORLD <sytnewworld@gmail.com>
2019-04-11 17:44:07 +0800

    修复警告

---
+++ CIComponentKitDemoTests/CIComponentKitDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  CIComponentKitDemoTests.m
+//  CIComponentKitDemoTests
+//
+//  Created by NEWWORLD on 2019/4/11.
+//  Copyright © 2019 codeinventor. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CIComponentKitDemoTests : XCTestCase
+
+@end
+
+@implementation CIComponentKitDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
